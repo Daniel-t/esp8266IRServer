2114fa6d8d5b4b13e11613de266e1792bbdc2e68
Daniel-t <dev+github@networklighthouse.com>
2017-03-04 06:59:19 +1000

    OBC for Samsung and NEC
    Added the ability to send commands with Original Button Codes (OBCs) for Samsung and NEC protocols

--- irServer.ino
+++ irServer.ino
@@ -5,8 +5,8 @@
 #include <IRremoteESP8266.h>
  
 // WLAN data 
-const char* ssid = "...";
-const char* password = "...";
+const char* ssid = "57";
+const char* password = "bar3H0use";
  
 // static IP
 IPAddress ip(192,168,0,201);
@@ -16,8 +16,8 @@ IPAddress subnet(255,255,255,0);
 MDNSResponder mdns;
 ESP8266WebServer server(80);
  
-// set your pin here  NOTE: Mapping to Arduino PIN
-IRsend irsend(2);
+// set your pin here
+IRsend irsend(4);
  
 void handleRoot() {
  server.send(200, "text/html", "Please specify command! Form: /ir?code=xxx&bits=xx&protocol=x");
@@ -27,18 +27,74 @@ void handleIr(){
   String codestring=server.arg("code");
   String protocol=server.arg("protocol");
   String bitsstring=server.arg("bits");
- 
-  String webOutput = "Protocol: "+protocol+"; Code: "+codestring+"; Bits: "+bitsstring;
+
+  String deviceCode=server.arg("deviceCode");
+  String subDeviceCode=server.arg("subDeviceCode");
+  String obc=server.arg("obc");
+
+//  String webOutput = "Protocol: "+protocol+"; Code: "+codestring+"; Bits: "+bitsstring + " - ("+deviceCode + subDeviceCode + obc +")";
+  String webOutput = "";
+  
   Serial.println(webOutput);
-    
+
+  unsigned long code =0;
+  
   if ((codestring != "")&&(bitsstring != "")){
     //unsigned long code = codestring.toInt();
     char tarray[15]; 
     codestring.toCharArray(tarray, sizeof(tarray));
-    unsigned long code = strtoul(tarray,NULL,16);  
+    code = strtoul(tarray,NULL,16);  
     // unsigned long code = atol(codestring);
+  }
+  
+  if ((obc != "")&&(deviceCode != "")){
+    //convert OBC & deviceCode to hex CodeString
+
+
+   int iDeviceCode=StrToUL(deviceCode);
+   int iDeviceCodeLSB=flipBits(iDeviceCode); 
+   int iDeviceCodeLSB_INV=iDeviceCodeLSB ^ 0xFF; 
+   dump(iDeviceCode, iDeviceCodeLSB, iDeviceCodeLSB_INV);
+   
+   int iSubDeviceCode;
+   int iSubDeviceCodeLSB;
+   int iSubDeviceCodeLSB_INV;
+    if ((subDeviceCode == "")){
+      iSubDeviceCode=iDeviceCode;
+      iSubDeviceCodeLSB=iDeviceCodeLSB; 
+      iSubDeviceCodeLSB_INV=iDeviceCodeLSB_INV; 
+    } else {
+       iSubDeviceCode=StrToUL(subDeviceCode);
+       iSubDeviceCodeLSB=flipBits(iSubDeviceCode); 
+       iSubDeviceCodeLSB_INV=iSubDeviceCodeLSB ^ 0xFF; 
+    }
+
+   int iOBC=StrToUL(obc);
+   int iOBCLSB=flipBits(iOBC); 
+   int iOBCLSB_INV=iOBCLSB ^ 0xFF; 
+   dump(iOBC,iOBCLSB,iOBCLSB_INV);
+   
+   Serial.println(iDeviceCodeLSB,HEX);
+   Serial.println(iOBCLSB,HEX);
+   Serial.println("----");
+   if (protocol=="Samsung") {
+    code=combineBytes(iDeviceCodeLSB,iSubDeviceCodeLSB,iOBCLSB,iOBCLSB_INV);
+   } else if (protocol=="NEC") {
+    code=combineBytes(iDeviceCodeLSB,iDeviceCodeLSB_INV,iOBCLSB,iOBCLSB_INV);
+   } else {
+    code=0;
+    server.send(404, "text/html", "Protocol not implemented for OBC!");
+   }
+   
+   Serial.println("---");
+   Serial.println(code,HEX);
+   Serial.println("---");
+   
+  }
+  
+  if (code!=0){
     int bits = bitsstring.toInt();
-    Serial.println(code);
+    Serial.println(code,HEX);
     
     if (protocol == "NEC"){
       server.send(200, "text/html", webOutput);
@@ -77,14 +133,41 @@ void handleIr(){
       irsend.sendSAMSUNG(code, bits);
     }
     else {
-      server.send(200, "text/html", "Protocol not implemented!");
+      server.send(404, "text/html", "Protocol not implemented!");
     }
   }
   else {
-    server.send(200, "text/html", "Missing code or bits!");
+    server.send(404, "text/html", "Missing code or bits!");
   }
 }
- 
+
+unsigned long StrToUL (String str){
+    char tarray[15]; 
+    str.toCharArray(tarray, sizeof(tarray));
+    unsigned long code = strtoul(tarray,NULL,10);  
+    return (code);
+}
+
+unsigned long combineBytes(int a1,int a2, int a3, int a4){
+   unsigned long code=0;
+   code=code+a1;
+   code=(code<<8)+a2;
+   code=(code<<8)+a3;
+   code=(code<<8)+a4;
+   return(code);
+}
+
+int flipBits(unsigned char b){
+  return ( (b * 0x0202020202ULL & 0x010884422010ULL) % 1023);
+}
+
+void dump(int a1,int a2,int a3){
+  Serial.print(a1,HEX);
+  Serial.print("-");
+  Serial.print(a2,HEX);
+  Serial.print("-");
+  Serial.println(a3,HEX);
+}
 void handleNotFound(){
   server.send(404, "text/plain", "404");
 }
